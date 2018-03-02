583fe8ed9fe32d9d530bf672f093310ec631262a
William N C Setten <33688252+WillSetten@users.noreply.github.com>
2018-03-02 10:42:13 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 #Add global constants into this file and import into all modules which require them
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+#5=easy, 3=medium, 1=hard. Corresponds to the damage multiplier the player is given for all damage dealt
+DIFFICULTY = 5
