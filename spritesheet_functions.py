8fd12623370cde4ce666fe01a6c41213c03701a1
William N C Setten <33688252+WillSetten@users.noreply.github.com>
2018-03-02 10:24:55 +0000

    Update spritesheet_functions.py

---
+++ spritesheet_functions.py
@@ -0,0 +1,2 @@
+#Used to pull a single sprite from a larger spritesheet. Method get_image required with 4 integer parameters corresponding to 
+#x,y position of sprite on sheet and the width and height of the sprite.
