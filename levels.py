2f19dc03cba5f0d3bb3e84cc4c28c3cf98195f99
Jason Jay Dookarun <jason.dookarun@live.co.uk>
2018-03-02 21:10:07 +0000

    Basic Structure for levels.py
    A basic structure has been formulated for further addition within the coming days.

---
+++ levels.py
@@ -0,0 +1,48 @@
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+
+class Level():
+ #This will be used as a super-class to define a level.
+
+    canvas_height = 720
+    canvas_width = 1280
+
+    def __init__(self,player):
+        #DATA WILL BE ADDED
+
+    def update(self):
+        #DATA WILL BE ADDDED
+
+    def draw(self,screen):
+        #Drawing bakground for this level.
+
+
+        screen.fill(constants.BLUE)
+        screen.blit(self.background,(self.world_shift // 3,0))
+
+        # Draw all the sprite lists that we will be using
+        self.platform_list.draw(screen)
+        self.enemy_list.draw(screen)
+
+    def shift_world(self, shift_x)
+    # This will relate to the movement of left/right when side scrolling
+
+    #Keep track of shift amount
+    self.world_shift += shift_x
+
+
+
+class Level_1(Level):
+
+    def __init__(self,player):
+
+        Level.__init__(self,player)
+        frame = simplegui.create_frame("Dash ‘em ‘n’ Smash ‘em", CANVAS_WIDTH, CANVAS_HEIGHT)
+        self.background = pygame.image.load(#Add image file here)
+        self.background.set_colourkey(constant.WHITE)
+
+
+frame.start()
