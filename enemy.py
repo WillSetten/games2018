9193e05e920ff0cdc833a4fa06ce4a86e519852d
tomsoal <33701079+tomsoal@users.noreply.github.com>
2018-03-04 16:01:42 +0000

    Update Enemy.py
    Have made the general foundations for this class, have highlighted points to consider on top couple lines of program, can discuss finer details of variables/logistics down the line.

---
+++ enemy.py
@@ -0,0 +1,83 @@
+import pygame, time
+#constructor should work by being called with an enemy type, then will set it up accordingly
+#assuming the main game class will select an enemy type and when to spawn it?
+#can discuss these issues in the week somewhen
+#Also not sure on how to track shots fired, whether to work them out in attacker class
+#or in the person who fired the shot
+class Enemy:
+    def __init__(self, type):/
+        if (type=="Infantry"):
+            self.health = 1
+            self.speed = 10 #value to be discussed later on
+            self.type = type
+            self.pos = Vector(0,0)
+            self.sprite = null #set as null for now while working on other areas
+        elif (type=="Heavy"):
+            self.health = 3
+            self.speed = 0
+            self.type = type
+            self.pos = Vector(0,0)
+            self.sprite = null
+        elif (type=="Drone"):
+            self.health = 3
+            self.speed = 5
+            self.type = type
+            self.pos = Vector(0,0)
+            self.sprite = null
+        else:
+            self.health = 40
+            self.speed = 0
+            self.type = type
+            self.pos = Vector(0,0)
+            self.sprite = null
+
+    def action(self):
+        spawn()
+        while (self.health>0):
+                if (self.type=="Infantry"):
+                    move()
+                    #check if the user is hit (use input from main class maybe/player fire class
+                    #if player shot is within range of player:
+                    #hit()
+                else:
+                    attack()
+                    #runs as per usual
+                    #if player shot hits enemy
+                        #hit()
+
+    def spawn(self):
+        #finds an empty platform - need implementation from platform class
+        #set self.pos to a place on platform off screen (account for sprite size etc)
+
+    def attack(self):
+        if (self.type=="Infantry"):#
+            #if distance from enemy to player is within a couple pixels, lose health from player
+        else:
+            #work out vector from enemy to player
+            #fire shot, work out sprite/object to be fired
+            #get to travel towards the player
+            #check if hits player
+
+    def move(self):
+        self.pos.x=self.pos.x+1
+        #only triggered if the enemy type is infantry
+
+    def hit(self):
+        self.health=self.health-1
+        if (self.health<1):
+            remove()
+            #if the enemy is dead, remove it from the game
+            if (self.type=="Boss"):
+                #score = score + 5000
+            elif (self.type=="Heavy"):
+                #score = score + 500
+            elif (self.type=="Drone"):
+                #score = score + 250
+            else:
+                #score = score + 100
+            #increment score since the user has killed an enemy
+        #potentially display different sprite if hit / blur sprite for x amount of time to indicate hit
+
+    def remove(self):
+        self.sprite = null #set to a blank sprite so it no longer appears
+        #will run if the enemy is dead, to remove them from the environment
