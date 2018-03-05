c700c4a7e012447349bb2c5c63d7da387496e1b6
William N C Setten <33688252+WillSetten@users.noreply.github.com>
2018-03-05 09:25:45 +0000

    Added and implemented the constants.py import

--- levels.py
+++ levels.py
@@ -4,13 +4,10 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 import pygame
-
+import constants
 
 class Level():
  #This will be used as a super-class to define a level.
-
-    constant.canvas_height = 720
-    constant.canvas_width = 1280
 
     def __init__(self,player):
         #list of all sprites need to be added here, after we have decided the sprites to be utilised
@@ -35,7 +32,7 @@ class Level():
 
     def draw(self,screen):
         #Drawing background for this level.
-        frame = simplegui.create_frame("Dash ‘em ‘n’ Smash ‘em", CANVAS_WIDTH, CANVAS_HEIGHT)
+        frame = simplegui.create_frame("Dash ‘em ‘n’ Smash ‘em", constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT)
 
         screen.fill(constants.BLUE)
         screen.blit(self.background,(self.world_shift // 3,0))
@@ -44,30 +41,27 @@ class Level():
         self.platform_list.draw(screen)
         self.enemy_list.draw(screen)
 
-    def shift_world(self, shift_x)
-    # This will relate to the movement of left/right when side scrolling
+    def shift_world(self, shift_x):
+        # This will relate to the movement of left/right when side scrolling
 
-    #Keep track of shift amount
-    self.world_shift += shift_x
+        #Keep track of shift amount
+        self.world_shift += shift_x
 
-    #Go through all the sprite lists and shift
-    for platform in self.platform_list:
-        platfor.rect.x += shift_x
+        #Go through all the sprite lists and shift
+        for platform in self.platform_list:
+            platform.rect.x += shift_x
 
-    for enemy in self.enemy_list:
-        enemy.rect.x += shift_x
+        for enemy in self.enemy_list:
+            enemy.rect.x += shift_x
 
 
 class Level_1(Level):
-
     def __init__(self,player):
 
         Level.__init__(self,player)
-        frame = simplegui.create_frame("Dash ‘em ‘n’ Smash ‘em", CANVAS_WIDTH, CANVAS_HEIGHT)
+        frame = simplegui.create_frame("Dash ‘em ‘n’ Smash ‘em", constants.SCREEN_WIDTH, constants.CANVAS_HEIGHT)
         self.background = pygame.image.load(#Add image file here)
-        self.background.set_colourkey(constant.WHITE)
-
+        #self.background.set_colourkey(constants.WHITE)
         #Application of moving platforms needs to be added
         #Array with type of platform, and x, y location of the platform, needs to be added
-
 frame.start()
