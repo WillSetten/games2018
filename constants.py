124c6ca52f4eedcefbea2c868196e2fc37e74357
William N C Setten <33688252+WillSetten@users.noreply.github.com>
2018-03-05 09:22:35 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,3 +3,6 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 #5=easy, 3=medium, 1=hard. Corresponds to the damage multiplier the player is given for all damage dealt
 DIFFICULTY = 5
+BLACK = (0,   0,   0) 
+WHITE = (255, 255, 255) 
+BLUE = (0,   0, 255)
