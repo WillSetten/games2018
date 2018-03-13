6c5a3d9d8800fef19d82bce4b60eb423f76207b5
Jason Jay Dookarun <jason.dookarun@live.co.uk>
2018-03-13 16:50:55 +0000

    RIP PHASE ONE

--- main.py
+++ main.py
@@ -1,84 +1,147 @@
-#main way of running the whole program
-#maybe use this to import sprite sheets and setup the whole game
+"""
+Sample Python/Pygame Programs
+Simpson College Computer Science
+http://programarcadegames.com/
+http://simpson.edu/computer-science/
 
+Main module for platform scroller example.
+
+From:
+http://programarcadegames.com/python_examples/sprite_sheets/
+
+Explanation video: http://youtu.be/czBDKWJqOao
+
+Part of a series:
+http://programarcadegames.com/python_examples/f.php?file=move_with_walls_example.py
+http://programarcadegames.com/python_examples/f.php?file=maze_runner.py
+http://programarcadegames.com/python_examples/f.php?file=platform_jumper.py
+http://programarcadegames.com/python_examples/f.php?file=platform_scroller.py
+http://programarcadegames.com/python_examples/f.php?file=platform_moving.py
+http://programarcadegames.com/python_examples/sprite_sheets/
+
+Game art from Kenney.nl:
+http://opengameart.org/content/platformer-art-deluxe
+
+"""
 import pygame
 import constants
 import levels
-import player
+import enemy
+
+from player import Player
 
 def main():
+    """ Main Program """
     pygame.init()
+
+    # Set the height and width of the screen
     size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
     screen = pygame.display.set_mode(size)
 
-    pygame.display.set_caption("Stick Warfare - Scrolling Action Game")
-    #initalises Player
+    pygame.display.set_caption("Artnoc")
+
+    # Create the player
     player = Player()
-    #creates all levels
+
+    # Create all the levels
     level_list = []
-    level_list.append(levels.Level_1(player))
-    #sets current Level
+    level_list.append(levels.Level_01(player))
+    #level_list.append(levels.Level_02(player))
+
+    # Set the current level
     current_level_no = 0
     current_level = level_list[current_level_no]
 
     active_sprite_list = pygame.sprite.Group()
-    player_level = current_level
+    player.level = current_level
 
     player.rect.x = 340
-    player.rect.y = constant.SCREEN_HEIGHT - player.rect.height
+    player.rect.y = constants.SCREEN_HEIGHT - player.rect.height
     active_sprite_list.add(player)
-    #loop until user quits the game
+
+    #Loop until the user clicks the close button.
     done = False
-    #manages how fast screen updates
+
+    # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
 
+    # -------- Main Program Loop -----------
     while not done:
-        for event in pygame.event.get(): #if user does something
-            if event.type == pygame.QUIT: #game closed
-                done = True
-            if event.type == pygame.KEYDOWN: #key pressed
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done = True # Flag that we are done so we exit this loop
+
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     player.prone()
                 if event.key == pygame.K_LEFT:
-                    player.moveLeft()
+                    player.go_left()
                 if event.key == pygame.K_RIGHT:
-                    player.moveRight()
-                if event_key == pygame.K_UP:
+                    player.go_right()
+                if event.key == pygame.K_UP:
+                    player.aimup()
+                if event.key == pygame.K_z:
                     player.jump()
+                if event.key == pygame.K_x and player.cooldown == 0:
+                    player.shoot()
 
-            if event.type == pygame.KEYUP: #key released
-                if event.key == pygame.K_LEFT and player.changeX < 0:
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT and player.change_x < 0:
                     player.stop()
-                if event.key == pygame.K_RIGHT and player.changeX > 0:
+                if event.key == pygame.K_RIGHT and player.change_x > 0:
                     player.stop()
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    player.resetaim()
 
-            #update player
-            active_sprite_list.update()
+        # Update the player.
+        active_sprite_list.update()
 
-            #update items in level
-            current_level.update()
+        #update the enemies
+        #for enemy in levels.Level.enemy_list:
+        #    enemy.update()
 
-            # If the player gets near the right side, shift the world left (-x)
-            if player.rect.right >= 500:
-                diff = player.rect.right - 500
-                player.rect.right = 500
-                current_level.shift_world(-diff)
+        # Update items in the level
+        current_level.update()
 
-            # If the player gets near the left side, shift the world right (+x)
-            if player.rect.left <= 120:
-                diff = 120 - player.rect.left
-                player.rect.left = 120
-                current_level.shift_world(diff)
-
+        # If the player gets near the right side, shift the world left (-x)
+        if player.rect.x >= 500:
+            diff = player.rect.x - 500
+            player.rect.x = 500
+            current_level.shift_world(-diff)
             current_level.draw(screen)
-            active_sprite_list.draw(screen)
 
-            #60fps
-            clock.tick(60)
+        # If the player gets near the left side, shift the world right (+x)
+        if player.rect.x <= 120:
+            diff = 120 - player.rect.x
+            player.rect.x = 120
+            current_level.shift_world(diff)
+            current_level.draw(screen)
 
-            #update SCREEN
-            pygame.display.flip()
-        pygame.quit()
+        # If the player gets to the end of the level, go to the next level
+        current_position = player.rect.x + current_level.world_shift
+        if current_position < current_level.level_limit:
+            player.rect.x = 120
+            if current_level_no < len(level_list)-1:
+                current_level_no += 1
+                current_level = level_list[current_level_no]
+                player.level = current_level
 
-    if __name__ == "__main__":
-        main()
+        # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+        current_level.draw(screen)
+        active_sprite_list.draw(screen)
+
+        # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+
+        # Limit to 60 frames per second
+        clock.tick(60)
+
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+    # Be IDLE friendly. If you forget this line, the program will 'hang'
+    # on exit.
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
