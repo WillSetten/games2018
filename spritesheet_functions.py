1b801700409ec87fbd3dadca7d8d0611b8c72fc6
William N C Setten <33688252+WillSetten@users.noreply.github.com>
2018-03-15 22:10:54 +0000

    updated spritesheet functions fetch

--- spritesheet_functions.py
+++ spritesheet_functions.py
@@ -3,9 +3,9 @@ import pygame
 import constants
 class SpriteSheet(object):
     def __init__(self, file_name):
-        self.sprite_sheet = pygame.image.load(file_name).convert()
+        self.sprite_sheet = pygame.image.load(file_name).convert_alpha()
         """Best way to use this module is at initialisation, where you can create the SpriteSheet then use the get_image
-        function in a for loop of some kind to add all the frames in a set animation to an array, then you can just 
+        function in a for loop of some kind to add all the frames in a set animation to an array, then you can just
         iterate through the array at initialisation"""
     def get_image(self, x, y, width, height):
         """x and y correspond to the position of the pixel in the top left of the image, width and height
