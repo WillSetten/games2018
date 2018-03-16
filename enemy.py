e458d584695395a4bd9eb3768ae4af87fcc38f42
William N C Setten <33688252+WillSetten@users.noreply.github.com>
2018-03-16 00:25:32 +0000

    Enemy Updates (incomplete)

--- enemy.py
+++ enemy.py
@@ -6,62 +6,144 @@ from vector import Vector
 from spritesheet_functions import SpriteSheet
 
 class Enemy(pygame.sprite.Sprite):
-
     #Attributes
+    #Melee type animations
+    walk_r = []
+    walk_l = []
+    die_M_r = []
+    die_M_l = []
+    #Ranged type frames
+    up_r=None
+    up_l= None
+    upangle_r = None
+    upangle_r = None
+    mid_r = None
+    mid_l = None
+    downangle_r = None
+    downangle_l = None
+    down_r = None
+    down_l = None
+    die_R_r = []
+    die_R_l = []
+    count = 0
     location = 0
     onPlatform = False
     change_x = 0
     change_y = 0
     level = None
-
+    direction = "L"
+    health = 0
+    type = 0
     def __init__(self, type):
 
         pygame.sprite.Sprite.__init__(self)
 
-        sprite_sheet = SpriteSheet("enemies1.png")
+        self.type = type
 
-        if (type=="Infantry"):
+        sprite_sheet = SpriteSheet("enemies1.png")
+        image = sprite_sheet.get_image(321, 1, 32, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(356, 1, 21, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(380, 1, 28, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(411, 1, 38, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(452, 1, 36, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(491, 1, 28, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(522, 1, 21, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(546, 1, 32, 47)
+        self.walk_r.append(image)
+        image = sprite_sheet.get_image(581, 1, 37, 47)
+        self.walk_r.append(image)
+        for x in self.walk_r[:]:
+            image = x
+            image = pygame.transform.flip(image, True, False)
+            self.walk_l.append(image)
+
+        self.up_r=sprite_sheet.get_image(321, 246, 25, 60)
+        self.up_l=pygame.transform.flip(self.up_r, True, False)
+        self.upangle_r = sprite_sheet.get_image(405, 250, 31, 56)
+        self.upangle_r = pygame.transform.flip(self.up_r, True, False)
+        self.mid_r = sprite_sheet.get_image(507, 261, 36, 45)
+        self.mid_l = pygame.transform.flip(self.up_r, True, False)
+        self.downangle_r = sprite_sheet.get_image(321, 312, 32, 45)
+        self.downangle_l = pygame.transform.flip(self.up_r, True, False)
+        self.down_r = sprite_sheet.get_image(426, 312, 25, 45)
+        self.down_l = pygame.transform.flip(self.up_r, True, False)
+
+        image = sprite_sheet.get_image(528, 216, 49, 38)
+        self.die_R_r.append(image)
+        image = sprite_sheet.get_image(528, 178, 49, 38)
+        self.die_R_r.append(image)
+        for x in self.die_R_r[:]:
+            image = x
+            image = pygame.transform.flip(image, True, False)
+            self.die_R_l.append(image)
+
+        image = sprite_sheet.get_image(321, 109, 37, 39)
+        self.die_M_r.append(image)
+        image = sprite_sheet.get_image(358, 109, 37, 39)
+        self.die_M_r.append(image)
+        for x in self.die_M_r[:]:
+            image = x
+            image = pygame.transform.flip(image, True, False)
+            self.die_M_l.append(image)
+
+        #0 = Melee 1 = Ranged
+        if (self.type==0):
             self.speed = 1
             self.health = 1
             self.type = type
             self.pos = Vector(0,0)
-            self.sprite = None #set as null for now while working on other areas
-        elif (type=="Heavy"):
+
+        elif (self.type==1):
             self.speed = 0.5
             self.health = 3
             self.type = type
             self.pos = Vector(0,0)
             self.sprite = None
-        else:
-            self.speed = 0.02
-            self.health = 40
-            self.type = type
-            self.pos = Vector(0,0)
-            self.sprite = None
 
-        self.image = sprite_sheet.get_image(30,0,30,40)#
-        self.image = pygame.transform.scale(self.image,(self.image.get_width()*constants.enemyscale,self.image.get_height()*constants.enemyscale))
+        self.image = sprite_sheet.get_image(30,0,60,40)
         self.rect = self.image.get_rect()
 
     def update(self, player):
-        if (self.type=="Infantry"):
-            self.move(player)
-            self.checkIfHit()
-
+        if (self.health<1):
+            #death animation
+            if (self.type=="0"):
+                if self.direction == "R":
+                    frame = (self.count) % len(self.die_M_r)
+                    self.image = self.die_M_r[frame]
+                else:
+                    frame = (self.count) % len(self.die_M_l)
+                    self.image = self.die_M_l[frame]
+            else:
+                if self.direction == "R":
+                    frame = (self.count) % len(self.die_R_r)
+                    self.image = self.die_R_r[frame]
+                else:
+                    frame = (self.count) % len(self.die_R_l)
+                    self.image = self.die_R_l[frame]
         else:
-            self.attack()
-            self.checkIfHit()
-
+            if (self.type=="0"):
+                self.move(player)
+                self.checkIfHit()
+            elif(self.type=="1"):
+                if(self.count%constants.ENEMYFIRERATE==0):
+                    aim_direction = Vector(self.rect.x-(main.player.rect.x+main.player.change_x),self.rect.y-(main.player.rect.y+main.player.change_y))
+                    compare = aim_direction.getP()
+                    aim_direction = aim_direction.normalize()
+                    if compare=="(0,0)":
+                        self.image = self.aim_mid_l
+                        #add a bullet to the enemy bullet list in main travelling in the direction aim_direction
+        self.count= self.count+1
     def spawn(self,x,y):
         self.rect.x = x
         self.rect.y = y
 
-
-    def attack(self):
-        pass
-            #work out vector from enemy to player
-            #fire shot, work out sprite/object to be fired
-            #need to consider power ups etc.
 
     def move(self, player):
         #gives them gravity physics so they fall if spawn in the sky
@@ -123,28 +205,5 @@ class Enemy(pygame.sprite.Sprite):
             self.change_y = 0
             self.rect.y = constants.SCREEN_HEIGHT - self.rect.height
 
-    def checkIfHit(self):
-    #need to set up code to work out whether enemy has been hit or not
-    #    block_hit_list = pygame.sprite.spritecollide(self, levels.Level.platform_list, False)
-    #    if block_hit_list != None:
-
-    #IF (Enemy has been hit)
-        self.health=self.health-1
-        if (self.health<1):
-            self.remove()
-        #if the enemy is dead, remove it from the game
-            if (self.type=="Boss"):
-                pass
-            #score = score + 5000
-            elif (self.type=="Heavy"):
-                pass
-                #score = score + 500
-            else:
-                pass
-                    #score = score + 100
-            #increment score since the user has killed an enemy
-        #potentially display different sprite if hit / blur sprite for x amount of time to indicate hit
-
-    def remove(self):
-        self.sprite = None #set to a blank sprite so it no longer appears
-        #will run if the enemy is dead, to remove them from the environment
+    def decreaseHealth(self):
+        pass
