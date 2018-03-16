d21ed7f854d802072f4213799d0d45da1000e720
tomsoal <tom.soal@live.co.uk>
2018-03-16 01:37:33 +0000

    WILL PUT "0" INSTEAD OF 0 LOLOLOLOL

--- enemy.py
+++ enemy.py
@@ -33,7 +33,8 @@ class Enemy(pygame.sprite.Sprite):
     level = None
     direction = "L"
     health = 0
-    type = 0
+    type = 0#
+
     def __init__(self, type):
 
         pygame.sprite.Sprite.__init__(self)
@@ -94,26 +95,26 @@ class Enemy(pygame.sprite.Sprite):
             self.die_M_l.append(image)
 
         #0 = Melee 1 = Ranged
-        if (self.type==0):
+        if (type==0):
             self.speed = 1
             self.health = 1
             self.type = type
             self.pos = Vector(0,0)
 
-        elif (self.type==1):
+        elif (type==1):
             self.speed = 0.5
             self.health = 3
             self.type = type
             self.pos = Vector(0,0)
             self.sprite = None
 
-        self.image = sprite_sheet.get_image(30,0,60,40)
+        self.image = self.walk_l[0]
         self.rect = self.image.get_rect()
 
     def update(self, player):
         if (self.health<1):
             #death animation
-            if (self.type=="0"):
+            if (self.type==0):
                 if self.direction == "R":
                     frame = (self.count) % len(self.die_M_r)
                     self.image = self.die_M_r[frame]
@@ -128,44 +129,20 @@ class Enemy(pygame.sprite.Sprite):
                     frame = (self.count) % len(self.die_R_l)
                     self.image = self.die_R_l[frame]
         else:
-            if (self.type=="0"):
+            if (self.type==0):
                 self.move(player)
-                self.checkIfHit()
-            elif(self.type=="1"):
+                #self.checkIfHit()
+            elif(self.type==1):
                 if(self.count%constants.ENEMYFIRERATE==0):
-                    aim_direction = Vector(self.rect.x-(main.player.rect.x+main.player.change_x),self.rect.y-(main.player.rect.y+main.player.change_y))
-                    compare = normaliseAngle(aim_direction.angle(Vector(0,self.rect.y)))
-                    if compare=="0,-1":
-                        if self.direction == "R":
-                            self.image = self.up_r
-                        else:
-                            self.image = self.up_l
-                    if compare=="1,-1":
-                        self.direction = "R"
-                        self.image = self.upangle_r
-                    if compare=="1,0":
-                        self.direction = "R"
-                        self.image = self.mid_r
-                    if compare=="1,1":
-                        self.direction = "R"
-                        self.image = self.downangle_r
-                    if compare=="0,1":
-                        if self.direction == "R":
-                            self.image = self.down_r
-                        else:
-                            self.image = self.down_l
-                    if compare=="-1,1":
-                        self.direction = "L"
-                        self.image = self.upangle_l
-                    if compare=="-1,0":
-                        self.direction = "L"
-                        self.image = self.mid_l
-                    if compare=="-1,-1":
-                        self.direction = "L"
-                        self.image = self.downangle_l
+                    aim_direction = Vector(self.rect.x-(player.rect.x+player.change_x),self.rect.y-(player.rect.y+player.change_y))
+                    compare = aim_direction.getP()
+                    aim_direction = aim_direction.normalize()
+                    if compare=="(0,0)":
+                        self.image = self.aim_mid_l
                         #add a bullet to the enemy bullet list in main travelling in the direction aim_direction
-        self.image = pygame.transform.scale(self.image,(self.image.get_width()*constants.enemyscale,self.image.get_height()*constants.enemyscale))
+
         self.count= self.count+1
+
     def spawn(self,x,y):
         self.rect.x = x
         self.rect.y = y
@@ -233,21 +210,3 @@ class Enemy(pygame.sprite.Sprite):
 
     def decreaseHealth(self):
         pass
-
-    def normaliseAngle(self,angle):
-        if(angle>22.5 or angle<=67.5):
-            return "1,-1"
-        if(angle>67.5 or angle<=112.5):
-            return "1,0"
-        if(angle>112.5 or angle<=157.5):
-            return "1,1"
-        if(angle>157.5 or angle<=202.5):
-            return "0,1"
-        if(angle>202.5 or angle<=247.5):
-            return "-1,1"
-        if(angle>247.5 or angle<=292.5):
-            return "-1,0"
-        if(angle>292.5 or angle<=337.5):
-            return "-1,-1"
-        else:
-            return "0,-1"
